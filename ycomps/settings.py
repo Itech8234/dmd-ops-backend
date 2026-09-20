efd0085179d392ba7ddd45b3d21cbcd05b76613b
OthmanSec <135832716+Itech8234@users.noreply.github.com>
2026-09-20 22:54:46 +0100

    Add 'dmd-ops-backend.onrender.com' to ALLOWED_HOSTS

--- ycomps/settings.py
+++ ycomps/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "1") == "1"
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,dmd-ops-backend.onrender.com").split(",")
 
 # ---------------------------------------------------------------------------
 # Applications
